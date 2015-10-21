89e421c5a2c19dc6adf47670ed1de73c955c1c73
jsw <jsw@3b1c9cea-dd9d-4dfb-bcb5-e65b7d4966e2>
2015-10-21 17:35:24 +0000

    Added ability to construct a Locality from a stream of Perturbs.

--- src/org/mitlware/mutable/Locality.java
+++ src/org/mitlware/mutable/Locality.java
@@ -3,9 +3,19 @@ package org.mitlware.mutable;
 import java.util.stream.Stream;
 
 @FunctionalInterface
-public interface Locality<S> {
+public interface Locality<State> {
 
-	public Stream<S> apply( S incumbent );	
+	public Stream<State> apply( State incumbent );
+	
+	///////////////////////////////
+	
+	public static <S> Locality<S> from( Stream< Perturb< S > > operators ) {
+		return new Locality< S >() {
+			public Stream<S> apply( S incumbent ) {
+				return operators.map( o -> o.apply( incumbent ) );
+			}
+		};
+	}
 }
 
 // End ///////////////////////////////////////////////////////////////
